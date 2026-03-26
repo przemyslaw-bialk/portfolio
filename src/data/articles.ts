7e1a0ebf34b71eb5f6e6db46f03dce3e50a178f7
przemek <przemek.bialkwno@gmail.com>
2026-03-26 17:37:26 +0100

    creating article about reduxtoolkit

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -12,6 +12,7 @@ import MongoWithNextjsEN from "../articles/MongoWithNextjsEN";
 import StaticVsDynamicNext from "../articles/StaticVsDynamicNext";
 import CachingInNext from "../articles/CachingInNext";
 import TypeScriptforReactEN from "../articles/TypeScriptForReactEN";
+import ReduxToolkitEN from "../articles/ReduxToolkitEN";
 
 export type ArticlesMap = Record<string, FC>;
 // [key: string] = typ obiektu typu string / FC = component
@@ -31,4 +32,5 @@ export const articlesMap: ArticlesMap = {
   static_vs_dynamic_in_next_EN: StaticVsDynamicNext,
   caching_in_next_EN: CachingInNext,
   typescript_for_react_EN: TypeScriptforReactEN,
+  redux_toolkit_EN: ReduxToolkitEN,
 };
